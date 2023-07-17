2d284d1e3d281ae76595190b0cca3337316b9c5c
Y <nguyenhoaiy7@gmail.com>
2023-07-17 15:48:26 +0700

    SCSS upadate

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express"); // Lệnh require lấy thư viện trong node_modules
+const app = express(); // Hàm có sẵn trong express, sẽ return đối tượng đại diện cho ứng dụng đang viết
+const port = 3000; // Chạy website ở cổng nào, ở đây là 3000
+const path = require("path");
+const morgan = require("morgan");
+
+console.log(__dirname); //C:\Users\Y\iCloudDrive\Documents\Visual Studio Code\JavaScript\NodeJS
+app.use(express.static(path.join(__dirname, "public")));
+
+// morgan hiển thị trạng thái gửi
+app.use(morgan("combined"));
+
+// Khai báo teamplate engines
+app.set("view engine", "pug");
+// Khai báo đường dẫn đến mục chứa template
+app.set("views", path.join(__dirname, "resources/views"));
+
+// Route để render trang web từ template Pug
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+// Lắng nghe cổng 3000 trên trình duyệt
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// http://localhost:3000
